b10d8cdfe22b8fa43ce162b9efe0fdf292ddd010
Shukri Adams <shukri.adams@gmail.com>
2022-02-16 18:01:12 +0100

    downgraded boto, scaffolded vagrant

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM ubuntu:20.04
 RUN apt-get update -y \
     && apt-get install duplicity -y \
     && apt-get install python3-pip -y \
-    && python3 -m pip install boto3
+    && python3 -m pip install boto
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,5 +6,11 @@ sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo usermod -aG docker vagrant
 
+# duplicate container contents
+sudo apt-get install duplicity -y 
+sudo apt-get install python3-pip -y
+sudo python3 -m pip install boto
+
+
 # force startup folder to vagrant project
 echo "cd /vagrant" >> /home/vagrant/.bashrc
